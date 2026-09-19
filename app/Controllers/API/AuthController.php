38128ab1dce1389ad9acb7c75393ec401f4ee4c4
HackYourWorldBae <rojjustind@gmail.com>
2026-09-19 20:18:13 +0800

    feat(auth): validate administrative and support building assignment for BSU staff

--- app/Controllers/API/AuthController.php
+++ app/Controllers/API/AuthController.php
@@ -141,6 +141,12 @@ class AuthController extends BaseController
                 } elseif (mb_strlen($college) > 150) {
                     $errors['college'] = ['College name must not exceed 150 characters.'];
                 }
+            } elseif ($employeeType === 'Support / Administrative Staff') {
+                if (empty($college)) {
+                    $errors['college'] = ['Please select your assigned administrative or support building.'];
+                } elseif (mb_strlen($college) > 150) {
+                    $errors['college'] = ['Building name must not exceed 150 characters.'];
+                }
             } else {
                 $college = null;
             }
